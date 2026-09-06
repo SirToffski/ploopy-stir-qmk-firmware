00905dce1d3d35d7d6c21f930469d6df116f036a
SirToffski <47118864+SirToffski@users.noreply.github.com>
2026-09-06 17:44:53 -0300

    Add files via upload

---
+++ keyboards/ploopyco/madromys/keymaps/via/rules.mk
@@ -0,0 +1,5 @@
+VIA_ENABLE = yes
+# Temporary for stir-to-scroll tuning: `qmk console` shows enter/exit + radius.
+# Remove once thresholds feel right.
+#CONSOLE_ENABLE = yes
+#RGBLIGHT_ENABLE = yes
